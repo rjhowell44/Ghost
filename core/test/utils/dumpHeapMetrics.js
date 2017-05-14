b519c8df4b8f48ba2c0737bfb3cf74584ee5b4ae
rjhowell44 <robert.howell@s2technologies.com>
2017-05-14 09:45:41 -0700

    fixed indents

--- core/test/utils/dumpHeapMetrics.js
+++ core/test/utils/dumpHeapMetrics.js
@@ -2,7 +2,7 @@ var heapMetrics = require('heap-metrics'),
     assert = require('assert');
 
 describe('DumpHeapMetrics', function () {
-  it('should return true', function () {
-    assert(heapMetrics.DumpHeapMetrics());
-  });
+    it('should return true', function () {
+        assert(heapMetrics.DumpHeapMetrics());
+    });
 });
