ab21ca04c2eaf1a92bc2d7ca736033dcd9f3d033
rjhowell44 <robert.howell@s2technologies.com>
2017-05-14 09:35:03 -0700

    Delete dump_metrics.js

--- core/test/dump_metrics.js
+++
@@ -1,9 +0,0 @@
-var heapMetrics = require('heap-metrics');
-var assert = require('assert');
-
-describe('DumpHeapMetrics', function(){
-  it('should return true', function(){
-    console.log(heapMetrics.GetHeapMetrics());
-    assert(heapMetrics.DumpHeapMetrics());
-  });
-})
